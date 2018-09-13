0479d0606d809bfb380877a1f1a42288e60d17fa
Nahuel <>
2018-09-13 09:39:28 +0100

    Kata1 v3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,16 +1,12 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1998,11,14);
-        Person person = new Person("Nahuel", date);
+        Person person = new Person("Nahuel", LocalDate.of(1998, Month.NOVEMBER, 14));
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
