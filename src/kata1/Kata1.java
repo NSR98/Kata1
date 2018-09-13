de93013b8186fe9c8149f1471246ef2dacdd9dcd
Nahuel <>
2018-09-13 10:08:52 +0100

    Kata1 v3.1
    Se realiza la misma función pero empleando la clase "LocalDateTime"
    que nos permite indicar una fecha de nacimiento más exacta.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,12 @@
 package kata1;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Nahuel", LocalDate.of(1998, Month.NOVEMBER, 14));
+        Person person = new Person("Nahuel", LocalDateTime.of(1998, Month.NOVEMBER, 14, 15, 30, 00));
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
